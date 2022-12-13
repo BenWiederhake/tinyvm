c33ef7367bb39f434d2061f6f6011780f037e884
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2022-12-13 15:00:15 +0100

    Assembler: Implement and test instruction Return

--- assembler/asm.py
+++ assembler/asm.py
@@ -2,9 +2,91 @@
 
 import sys
 
+SEGMENT_LENGTH = 131072
+ASM_COMMANDS = dict()
+DEBUG_OUTPUT = False
+ERROR_OUTPUT = True
+
+
+def asm_command(fn):
+    name = fn.__name__
+    prefix = "parse_command_"
+    assert name.startswith(prefix), name
+    command_name = name[len(prefix) :]
+    global ASM_COMMANDS
+    assert command_name not in ASM_COMMANDS
+    ASM_COMMANDS[command_name] = fn
+    return fn
+
+
+class Assembler:
+    def __init__(self):
+        self.segment_words = [0] * (SEGMENT_LENGTH // 2)
+        self.current_lineno = None
+        self.current_pointer = 0x0000
+
+    def error(self, msg):
+        if DEBUG_OUTPUT or ERROR_OUTPUT:
+            print(f"line {self.current_lineno}: {msg}", file=sys.stderr)
+            # TODO: Do something more clever?
+        return False
+
+    def push_word(self, word):
+        if DEBUG_OUTPUT:
+            print(f"  pushing {word:04X}")
+        assert 0 <= word <= 0xFFFF
+        if self.segment_words[self.current_pointer] != 0:
+            return self.error(
+                f"Would overwrite word {self.segment_words[self.current_pointer]} at {self.current_pointer:04X}."
+            )
+        self.segment_words[self.current_pointer] = word
+        self.current_pointer += 1
+        self.current_pointer %= SEGMENT_LENGTH // 2
+        return True
+
+    @asm_command
+    def parse_command_ret(self, command, args):
+        if args != "":
+            return self.error(
+                f"Command 'ret' does not take any arguments (expected end of line, found '{args}' instead)"
+            )
+        return self.push_word(0x102A)
+
+    def parse_line(self, line, lineno):
+        self.current_lineno = lineno
+        line = line.strip()
+        if not line or line.startswith("#"):
+            # Nothing to do here
+            return True
+        if " " in line:
+            command, args = line.split(" ", 1)
+        else:
+            command = line
+            args = ""
+        if DEBUG_OUTPUT:
+            print(f"{lineno}: {line}  # {command=} {args=}")
+        command_fn = ASM_COMMANDS.get(command)
+        if command_fn is None:
+            return self.error(
+                f"Command '{command}' not found. Did you mean any of {ASM_COMMANDS.keys()}' instead?"
+            )
+
+        return command_fn(self, command, args)
+
+    def segment_bytes(self):
+        segment = bytearray(SEGMENT_LENGTH)
+        for i, word in enumerate(self.segment_words):
+            segment[2 * i] = word >> 8
+            segment[2 * i + 1] = word & 0xFF
+        return bytes(segment)
+
 
 def compile_to_segment(asm_text):
-    return b"\x00" * 131072
+    asm = Assembler()
+    for i, line in enumerate(asm_text.split("\n")):
+        if not asm.parse_line(line, i):
+            return None
+    return asm.segment_bytes()
 
 
 def run_on_files(infile, outfile):
--- assembler/asm_test.py
+++ assembler/asm_test.py
@@ -4,8 +4,6 @@ from collections import Counter
 import asm
 import unittest
 
-SEGMENT_LENGTH = 131072
-
 ASM_TESTS = [
     ("empty", "", ""),
     (
@@ -21,13 +19,21 @@ ASM_TESTS = [
         """,
         "",
     ),
+    (
+        "return",
+        """
+        ret
+        """,
+        "102A",
+    ),
 ]
 
 
 class AsmTests(unittest.TestCase):
     def test_empty(self):
-        self.assertEqual(b"\x00" * SEGMENT_LENGTH, asm.compile_to_segment(""))
-        self.assertEqual(b"\x00" * SEGMENT_LENGTH, asm.compile_to_segment("\n"))
+        asm.ERROR_OUTPUT = False
+        self.assertEqual(b"\x00" * asm.SEGMENT_LENGTH, asm.compile_to_segment(""))
+        self.assertEqual(b"\x00" * asm.SEGMENT_LENGTH, asm.compile_to_segment("\n"))
 
     def test_testsuite_names(self):
         nameCounter = Counter(name for name, _, _ in ASM_TESTS)
@@ -36,12 +42,13 @@ class AsmTests(unittest.TestCase):
                 self.assertEqual(count, 1)
 
     def test_hardcoded(self):
+        asm.ERROR_OUTPUT = False
         for i, (name, asm_text, code_prefix_hex) in enumerate(ASM_TESTS):
             with self.subTest(i=i, name=name):
                 expected_segment = bytearray.fromhex(code_prefix_hex)
-                self.assertTrue(len(expected_segment) <= SEGMENT_LENGTH)
+                self.assertTrue(len(expected_segment) <= asm.SEGMENT_LENGTH)
                 self.assertEqual(len(expected_segment) % 2, 0)
-                padding = b"\x00" * (SEGMENT_LENGTH - len(expected_segment))
+                padding = b"\x00" * (asm.SEGMENT_LENGTH - len(expected_segment))
                 expected_segment.extend(padding)
                 self.assertEqual(expected_segment, asm.compile_to_segment(asm_text))
 
