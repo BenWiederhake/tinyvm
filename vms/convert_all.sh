0e8b037dfdac4cc9ca9f166997fea442056f6d4b
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2023-06-28 00:54:33 +0200

    CI+VM: Automatically generate map files for all VMs

--- vms/convert_all.sh
+++ vms/convert_all.sh
@@ -11,6 +11,7 @@ for ASM_FILENAME in */*.asm; do
         echo "Weird asm filename?! ${ASM_FILENAME}"
         continue
     fi
-    echo "${ASM_FILENAME} -> ${SEGMENT_FILENAME}"
-    "${ASSEMBLER}" "${ASM_FILENAME}" "${SEGMENT_FILENAME}"
+    MAP_FILENAME="$(echo "${ASM_FILENAME}" | sed -Ee 's/.asm$/.map.json/')"
+    echo "${ASM_FILENAME} -> ${SEGMENT_FILENAME} ${MAP_FILENAME}"
+    "${ASSEMBLER}" "${ASM_FILENAME}" "${SEGMENT_FILENAME}" "${MAP_FILENAME}"
 done
